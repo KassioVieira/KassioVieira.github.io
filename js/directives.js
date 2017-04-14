4e512ceb538afc7db2bbe8cc7d1c72a9e01e04f0
KassioVieira <kassio.vieira7@gmail.com>
2017-04-14 00:07:28 -0300

    estrutura do blog

---
+++ js/directives.js
@@ -0,0 +1,15 @@
+angular.module("app").directive("mastHead", function() {
+  return {
+    templateUrl: 'template/mast-head.html'
+  };
+});
+angular.module("app").directive("mastFooter", function() {
+  return {
+    templateUrl: 'template/mast-footer.html'
+  };
+});
+angular.module("app").directive("mastAbout", function() {
+  return {
+    templateUrl: 'template/mast-about.html'
+  };
+});
