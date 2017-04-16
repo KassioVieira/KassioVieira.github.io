58ec79ce3407259a6a9c34bda3d1397d587f2214
KassioVieira <kassio.vieira7@gmail.com>
2017-04-16 14:39:23 -0300

    adicionando arquivo de template

--- js/directives.js
+++ js/directives.js
@@ -3,13 +3,16 @@ angular.module("app").directive("mastHead", function() {
     templateUrl: 'template/mast-head.html'
   };
 });
+
+
+angular.module("app").directive("mastSobre", function() {
+  return {
+    templateUrl: 'template/mast-sobre.html'
+  };
+});
+
 angular.module("app").directive("mastFooter", function() {
   return {
     templateUrl: 'template/mast-footer.html'
-  };
-});
-angular.module("app").directive("mastAbout", function() {
-  return {
-    templateUrl: 'template/mast-about.html'
   };
 });
